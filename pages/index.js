cdb0512c7e8bb4c643fe97b57c31c398a5e11023
Fabricio <contact@fabricioguardia.com>
2021-04-21 13:50:55 -0700

    modified login page

--- pages/index.js
+++ pages/index.js
@@ -59,13 +59,13 @@ const Home = ({}) => {
         <div className="home-wave">
           <div className="waveWrapper waveAnimation">
             <div className="waveWrapperInner bgTop">
-              <div className="wave waveTop" style={{'background-image': 'url("http://front-end-noobs.com/jecko/img/wave-top.png")'}}></div>
+              <div className="wave waveTop" style={{'backgroundImage': 'url("http://front-end-noobs.com/jecko/img/wave-top.png")'}}></div>
             </div>
             <div className="waveWrapperInner bgMiddle">
-              <div className="wave waveMiddle" style={{'background-image': 'url("http://front-end-noobs.com/jecko/img/wave-mid.png")'}}></div>
+              <div className="wave waveMiddle" style={{'backgroundImage': 'url("http://front-end-noobs.com/jecko/img/wave-mid.png")'}}></div>
             </div>
             <div className="waveWrapperInner bgBottom">
-              <div className="wave waveBottom" style={{'background-image': 'url("http://front-end-noobs.com/jecko/img/wave-bot.png")'}}></div>
+              <div className="wave waveBottom" style={{'backgroundImage': 'url("http://front-end-noobs.com/jecko/img/wave-bot.png")'}}></div>
             </div>
           </div>
         </div>
---
+++ pages/mixer.js
@@ -0,0 +1,11 @@
+
+const Mixer = ({}) => {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Mixer
