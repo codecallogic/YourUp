8a1e689bc7d41660e5fa6ee5d3032661fea75804
Fabricio <contact@fabricioguardia.com>
2021-04-21 18:19:45 -0700

    meta tag update

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <>
-      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"></meta>
       <link rel="stylesheet" href="https://use.typekit.net/xne7ixk.css"></link>
       <link rel="stylesheet" href="https://use.typekit.net/xne7ixk.css"></link>
       <link rel="stylesheet" href="https://use.typekit.net/xne7ixk.css"></link>
