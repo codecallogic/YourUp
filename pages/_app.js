37a2e3c6178562b9ddc5e626d883f46bccfac446
Fabricio <contact@fabricioguardia.com>
2021-04-21 18:34:31 -0700

    moved app.css file

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import '../styles/app.css'
+import './app.css'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <>
+      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"></meta>
       <link rel="stylesheet" href="https://use.typekit.net/xne7ixk.css"></link>
       <link rel="stylesheet" href="https://use.typekit.net/xne7ixk.css"></link>
       <link rel="stylesheet" href="https://use.typekit.net/xne7ixk.css"></link>
