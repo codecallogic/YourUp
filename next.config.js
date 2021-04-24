5cdf9d2044f191333e75a89df8fa12056b9f55db
Fabricio <contact@fabricioguardia.com>
2021-04-24 00:27:20 -0700

    variable update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 module.exports = ({
   publicRuntimeConfig: {
     APP_NAME: 'YourUp',
-    API: 'http://54.151.3.30//api',
+    API: 'http://54.151.3.30/api',
     PRODUCTION: false,
     DOMAIN: 'http://54.151.3.30/',
   }
 })
+
+// module.exports = ({
+//   publicRuntimeConfig: {
+//     APP_NAME: 'YourUp',
+//     API: 'http://localhost:3001/api',
+//     PRODUCTION: false,
+//     DOMAIN: 'http://localhost:3000/',
+//   }
+// })
